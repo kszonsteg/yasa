93a7d7134824afe65c1c0570d9f06bf9cbdec60b
kszonsteg <kszonsteg@gmail.com>
2025-12-30 14:19:36 +0100

    refactor(rust): set parent action on select

--- src/actions/execution/turn.rs
+++ src/actions/execution/turn.rs
@@ -11,6 +11,7 @@ pub fn start_move_execution(game_state: &mut GameState, action: &Action) -> Resu
             .clone(),
     );
     game_state.procedure = Some(Procedure::MoveAction);
+    game_state.parent_procedure = Some(Procedure::MoveAction);
     Ok(())
 }
 
@@ -23,6 +24,7 @@ pub fn start_blitz_execution(game_state: &mut GameState, action: &Action) -> Res
             .clone(),
     );
     game_state.procedure = Some(Procedure::BlitzAction);
+    game_state.parent_procedure = Some(Procedure::BlitzAction);
     game_state
         .turn_state
         .as_mut()
@@ -40,6 +42,7 @@ pub fn start_pass_execution(game_state: &mut GameState, action: &Action) -> Resu
             .clone(),
     );
     game_state.procedure = Some(Procedure::PassAction);
+    game_state.parent_procedure = Some(Procedure::PassAction);
     game_state
         .turn_state
         .as_mut()
@@ -57,6 +60,7 @@ pub fn start_handoff_execution(game_state: &mut GameState, action: &Action) -> R
             .clone(),
     );
     game_state.procedure = Some(Procedure::HandoffAction);
+    game_state.parent_procedure = Some(Procedure::HandoffAction);
     game_state
         .turn_state
         .as_mut()
@@ -74,6 +78,7 @@ pub fn start_foul_execution(game_state: &mut GameState, action: &Action) -> Resu
             .clone(),
     );
     game_state.procedure = Some(Procedure::FoulAction);
+    game_state.parent_procedure = Some(Procedure::FoulAction);
     game_state
         .turn_state
         .as_mut()
@@ -91,12 +96,14 @@ pub fn start_block_execution(game_state: &mut GameState, action: &Action) -> Res
             .clone(),
     );
     game_state.procedure = Some(Procedure::BlockAction);
+    game_state.parent_procedure = Some(Procedure::BlockAction);
     Ok(())
 }
 
 pub fn end_turn_execution(game_state: &mut GameState) -> Result<(), String> {
     game_state.active_player_id = None;
     game_state.procedure = Some(Procedure::EndTurn);
+    game_state.parent_procedure = None;
     Ok(())
 }
 
@@ -108,5 +115,6 @@ pub fn end_player_turn_execution(game_state: &mut GameState) -> Result<(), Strin
     active_player.state.has_blocked = false;
     game_state.active_player_id = None;
     game_state.procedure = Some(Procedure::Turn);
+    game_state.parent_procedure = None;
     Ok(())
 }
